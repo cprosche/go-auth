29698b4216ab82dd3ad8fb4487417319363441a7
Cade Rosche <caderosche@gmail.com>
2022-09-05 11:16:26 -0500

    hasing passwords in register route

--- main.go
+++ main.go
@@ -7,11 +7,20 @@ import (
 
 	"github.com/gin-gonic/gin"
 	env "github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
 
 	"database/sql"
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+// nodemon --exec go run main.go --ext go
+
+// TODO: login route
+// TODO: password validation
+// TODO: ip address restriction? maybe on deployment server not app
+// TODO: jwt token return in login route (edcsa public private key pair?)
+// TODO: set up with pscale
 
 type User struct {
 	ID       int    `json:"id"`
@@ -23,9 +32,10 @@ type User struct {
 func main() {
 	loadEnv()
 	router := gin.Default()
+
 	router.GET("/users", getAllUsers)
 	router.POST("/register", createNewUser)
-	fmt.Println("Listening at http://localhost:8080")
+
 	router.Run("localhost:8080")
 }
 
@@ -67,25 +77,41 @@ func getAuthDbConnection() *sql.DB {
 }
 
 func createNewUser(context *gin.Context) {
+	// get the new user from the request body
 	var newUser User
 	err := context.BindJSON(&newUser)
 	if err != nil {
-		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": "malformed request"})
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"error": "malformed request"})
 		return
 	}
 
+	// generate a hash from the password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newUser.Pw), bcrypt.DefaultCost)
+	if err != nil {
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"error": "password hashing failed"})
+		return
+	}
+
+	// connect to db
 	db := getAuthDbConnection()
 	defer db.Close()
 
+	// insert user into db
 	sql := "INSERT INTO users(username, email, pw) VALUES (?, ?, ?)"
-	_, err = db.Exec(sql, newUser.Username, newUser.Email, newUser.Pw)
+	_, err = db.Exec(sql, newUser.Username, newUser.Email, hashedPassword)
 	if err != nil {
 		context.IndentedJSON(http.StatusBadRequest, err)
 		return
 	}
 
+	// query new user from db to return new id
 	row := db.QueryRow("SELECT id, username, email, pw FROM users WHERE username = ?", newUser.Username)
-	row.Scan(&newUser.ID, &newUser.Username, &newUser.Email, &newUser.Pw)
+	err = row.Scan(&newUser.ID, &newUser.Username, &newUser.Email, &newUser.Pw)
+	if err != nil {
+		context.IndentedJSON(http.StatusBadRequest, err)
+		return
+	}
 
-	context.IndentedJSON(http.StatusAccepted, newUser)
+	// return new user id to client
+	context.IndentedJSON(http.StatusAccepted, newUser.ID)
 }
